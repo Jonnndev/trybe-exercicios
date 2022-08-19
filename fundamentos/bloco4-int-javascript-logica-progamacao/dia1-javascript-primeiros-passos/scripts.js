dcbe84f20438a99f1b88ca5cf50fb3d16f62ef4d
Jonathas Assis de Oliveira <jonnutri@gmail.com>
2022-08-19 11:31:44 -0300

    exercicio matéria - completos

--- fundamentos/bloco4-int-javascript-logica-progamacao/dia1-javascript-primeiros-passos/scripts.js
+++ fundamentos/bloco4-int-javascript-logica-progamacao/dia1-javascript-primeiros-passos/scripts.js
@@ -1,8 +1,16 @@
+// ATV1
 const myName = 'Jonathas';
 console.log(myName);
-
+// ATV2
 const birthCity = 'Ipatinga';
 console.log(birthCity);
-
+// ATV3
 let birthYear = 1994;
 console.log(birthYear);
+// ATV5
+birthYear = 1996;
+console.log(birthYear);
+// ATV6
+birthCity = 1996;
+console.log(birthCity);
+// O erro se dá porque não é possivel reatribuir uma CONSTANTE
