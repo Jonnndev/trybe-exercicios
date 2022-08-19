6112449249ef2b92ce7eb0605867f6b3a01fc6a9
Jonathas Assis de Oliveira <jonnutri@gmail.com>
2022-08-19 11:28:51 -0300

    exercicio matéria - criar const nome e cidade / criar let ano

--- fundamentos/bloco4-int-javascript-logica-progamacao/dia1-javascript-primeiros-passos/scripts.js
+++ fundamentos/bloco4-int-javascript-logica-progamacao/dia1-javascript-primeiros-passos/scripts.js
@@ -1,2 +1,8 @@
-let myName = 'Jonathas';
+const myName = 'Jonathas';
 console.log(myName);
+
+const birthCity = 'Ipatinga';
+console.log(birthCity);
+
+let birthYear = 1994;
+console.log(birthYear);
