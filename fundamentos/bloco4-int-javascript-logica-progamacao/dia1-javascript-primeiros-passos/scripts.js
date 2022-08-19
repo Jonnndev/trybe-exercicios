6e203a7f9cb931f87bd8668d13a096b48aa62c7f
Jonathas Assis de Oliveira <jonnutri@gmail.com>
2022-08-19 12:43:00 -0300

    exercicio matéria PT2 - completos

--- fundamentos/bloco4-int-javascript-logica-progamacao/dia1-javascript-primeiros-passos/scripts.js
+++ fundamentos/bloco4-int-javascript-logica-progamacao/dia1-javascript-primeiros-passos/scripts.js
@@ -1,16 +1,28 @@
-// ATV1
-const myName = 'Jonathas';
-console.log(myName);
-// ATV2
-const birthCity = 'Ipatinga';
-console.log(birthCity);
-// ATV3
-let birthYear = 1994;
-console.log(birthYear);
-// ATV5
-birthYear = 1996;
-console.log(birthYear);
-// ATV6
-birthCity = 1996;
-console.log(birthCity);
-// O erro se dá porque não é possivel reatribuir uma CONSTANTE
+// // ATV1
+// const myName = 'Jonathas';
+// console.log(myName);
+// // ATV2
+// const birthCity = 'Ipatinga';
+// console.log(birthCity);
+// // ATV3
+// let birthYear = 1994;
+// console.log(birthYear);
+// // ATV5
+// birthYear = 1996;
+// console.log(birthYear);
+// // ATV6
+// // birthCity = 1996;
+// // console.log(birthCity);
+// // O erro se dá porque não é possivel reatribuir uma CONSTANTE
+// // ATV7 - Retangulo
+let base = 5;
+let height = 8;
+
+const area = base * height;
+console.log(area);
+
+const perimeter = (base * 2) + (height * 2);
+console.log(perimeter);
+
+
+
