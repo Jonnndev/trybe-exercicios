2a0a08ce061aa2833476fae96859ed8483e97aed
Jonathas Assis de Oliveira <jonnutri@gmail.com>
2022-08-19 13:51:24 -0300

    exercicio do conteudo completos

--- fundamentos/bloco4-int-javascript-logica-progamacao/dia1-javascript-primeiros-passos/scripts.js
+++ fundamentos/bloco4-int-javascript-logica-progamacao/dia1-javascript-primeiros-passos/scripts.js
@@ -1,3 +1,6 @@
+// ATIVIDADE PRÁTICA DO DIA
+
+// ATIVIDADE DO CONTEÚDO
 // // ATV1
 // const myName = 'Jonathas';
 // console.log(myName);
@@ -15,14 +18,66 @@
 // // console.log(birthCity);
 // // O erro se dá porque não é possivel reatribuir uma CONSTANTE
 // // ATV7 - Retangulo
-let base = 5;
-let height = 8;
+// let base = 5;
+// let height = 8;
 
-const area = base * height;
-console.log(area);
+// const area = base * height;
+// console.log(area);
 
-const perimeter = (base * 2) + (height * 2);
-console.log(perimeter);
+// const perimeter = (base * 2) + (height * 2);
+// console.log(perimeter);
+// const nota = 70;
 
+// if (nota >= 80){
+//   console.log("Parabéns, você foi aprovada(o)!");
+// }
+// else if (nota >= 60){
+//   console.log("Você está na nossa lista de espera.");
+// }
+// else {
+//     console.log("Você foi reprovad(o)!");
+// }
+// // ATV8 - &&
+// const currentHour = 4;
+// let message = '';
 
+// if (currentHour >= 22){
+//   message = 'Não deveríamos comer nada, é hora de dormir'
+// }
+// else if (currentHour >= 18){
+//   message = 'Rango da noite, vamos jantar :D'
+// }
+// else if (currentHour >= 14){
+//   message = 'Vamos fazer um bolo pro café da tarde?'
+// }
+// else if (currentHour >= 11){
+//   message = 'Hora do almoço!!!';
+// }
+// else if (currentHour >= 4) {
+//   message = "Hmmm, cheiro de café recém passado" na variável';
+// }
+// console.log(message);
+// // ATV9 - ||
+// let weekDay = 'sabado';
 
+// if (weekDay === 'segunda-feira' || weekDay === 'terça-feira' || weekDay === 'quarta-feira' || weekDay === 'quinta-feira' || weekDay === 'sexta-feira'){
+//   console.log('Oba, mais um dia de aprendizado na Trybe >:D');
+// }
+// else {
+//   console.log('FINALMENTE, descanso merecido UwU');
+// }
+// // ATV10 - SWITCH/CASE
+// let candidato = '';
+// switch (candidato){
+//   case 'aprovado':
+//     console.log('Parabéns, você foi aprovado(a)!');
+//     break;
+//   case 'lista':
+//     console.log('Você está em nossa lista de espera.');
+//     break;
+//   case 'reprovado':
+//     console.log('Você foi reprovado(a), infelizmente.');
+//     break;
+//   default:
+//     console.log('Não se aplica');
+// }
