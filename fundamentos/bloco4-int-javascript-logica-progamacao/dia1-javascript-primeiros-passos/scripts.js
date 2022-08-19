6a5bf594c4fa125f08c9a5735762a9b7cd720585
Jonathas Assis de Oliveira <jonnutri@gmail.com>
2022-08-19 14:02:00 -0300

    Atividade prática do dia 4.1 - EX1 feito

--- fundamentos/bloco4-int-javascript-logica-progamacao/dia1-javascript-primeiros-passos/scripts.js
+++ fundamentos/bloco4-int-javascript-logica-progamacao/dia1-javascript-primeiros-passos/scripts.js
@@ -19,6 +19,12 @@ console.log(adicao);
 adicao = a % b;
 console.log(adicao);
 
+// 2- Faça um programa que retorne o maior de dois números. Defina no começo do programa duas constantes com os valores que serão comparados.
+
+
+
+
+
 // ATIVIDADE DO CONTEÚDO
 // // ATV1
 // const myName = 'Jonathas';
