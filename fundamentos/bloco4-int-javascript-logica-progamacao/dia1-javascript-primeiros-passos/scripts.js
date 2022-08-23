ea8136351fbfdbc4e8094ad9650b0b7dbf929deb
Jonathas Assis de Oliveira <jonnutri@gmail.com>
2022-08-23 14:54:28 -0300

    Resoluções only

--- fundamentos/bloco4-int-javascript-logica-progamacao/dia1-javascript-primeiros-passos/scripts.js
+++ fundamentos/bloco4-int-javascript-logica-progamacao/dia1-javascript-primeiros-passos/scripts.js
@@ -1,5 +1,5 @@
 // ATIVIDADE PRÁTICA DO DIA
-// 1 - Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b, definidas no começo com os valores que serão operados. Faça programas para:
+// ATIVIDADE 1
 // let a = 10;
 // let b = 4;
 // let adicao;
@@ -19,7 +19,7 @@
 // adicao = a % b;
 // console.log(adicao);
 
-// 2- Faça um programa que retorne o maior de dois números. Defina no começo do programa duas constantes com os valores que serão comparados.
+// ATIVIDADE 2
 // const x = 20;
 // const y = 86;
 
@@ -30,7 +30,7 @@
 //   console.log('A variável Y vale ' + y + ' e é maior que X');
 // }
 
-// 3- Faça um programa que retorne o maior de três números. Defina no começo do programa três constantes com os valores que serão comparados.
+// ATIVIDADE 3
 // const j = 54;
 // const k = 230;
 // const l = 40
@@ -45,7 +45,7 @@
 //   console.log('A variável l vale ' + l + ' e é maior que j e k');
 // }
 
-// 4- Faça um programa que, dado um valor recebido como parâmetro, retorne "positive" se esse valor for positivo, "negative" se for negativo, e caso não seja nem positivo e nem negativo retorne "zero".
+// ATIVIDADE 4
 // let parameter;
 // parameter = 0;
 
@@ -59,9 +59,7 @@
 //   console.log('zero');
 // }
 
-// 5- Faça um programa que defina três variáveis com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false, caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro. 
-// -Para os ângulos serem de um triângulo válido, a soma dos três devem ser 180 graus.
-// -Um ângulo será considerado inválido se não tiver um valor positivo.
+// ATIVIDADE 5
 // let a = 30;
 // let b = 95;
 // let c = 55;
@@ -76,11 +74,7 @@
 //   console.log('Ops, é ' + triangulo + '. Não é um triângulo!!');
 // }
 
-// 6 - Escreva um programa que receba o nome de uma peça de xadrez e retorne os movimentos que ela faz.
-// Como desafio, faça o programa funcionar tanto se receber o nome de uma peça com letras maiúsculas quanto com letras minúsculas, sem aumentar a quantidade de condicionais.
-// Como dica, você pode pesquisar uma função que faz uma string ficar com todas as letras minúsculas (lower case).
-// Se a peça passada for inválida, o programa deve retornar uma mensagem de erro.
-// Exemplo: bishop (bispo) -> diagonals (diagonais)
+/// ATIVIDADE 6
 // let nomePeça;
 // nomePeça =  'PEÃO'
 
@@ -107,7 +101,7 @@
 //     console.log('error - peça não existente!')
 // }
 
-// 7 - Escreva um programa que converte uma nota dada em porcentagem (de 0 a 100) em conceitos de A a F. Siga essas regras:
+// ATIVIDADE 7
 // Porcentagem >= 90 -> A
 // Porcentagem >= 80 -> B
 // Porcentagem >= 70 -> C
@@ -140,8 +134,7 @@
 //   console.log('Erro na nota.')
 // }
 
-// 8 - Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for par. Caso contrário, ele retorna false.
-// Bonus: use somente um if.
+// ATIVIDADE 8
 // const a = 72;
 // const b = 36;
 // const c = 48;
@@ -153,8 +146,7 @@
 //   console.log('False, nenhum deles é par.')
 // }
 
-// 9 - Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for ímpar. Caso contrário, ele retorna false.
-// Bonus: use somente um if.
+// ATIVIDADE 9
 // const a = 2;
 // const b = 6;
 // const c = 3;
@@ -166,11 +158,7 @@
 //   console.log('False, todoas os números são par.')
 // }
 
-// 10 - Escreva um programa que se inicie com dois valores em duas constantes diferentes: o custo de um produto e seu valor de venda. A partir dos valores, calcule quanto de lucro (valor de venda descontado o custo do produto) a empresa terá ao vender mil desses produtos.
-// Atente que, sobre o custo do produto, incide um imposto de 20%.
-// Seu programa também deve emitir uma mensagem de erro e encerrar caso algum dos seus valores de entrada seja menor que zero.
-// O lucro de um produto é o resultado da subtração do valor de venda pelo custo do mesmo, sendo que o imposto de 20% também faz parte do valor de custo.
-// valorCustoTotal = valorCusto + impostoSobreOCusto
+// ATIVIDADE 10
 // lucro = valorVenda - valorCustoTotal (lucro de um produto)
 // const custo = 55;
 // let imposto = 0.20;
@@ -186,19 +174,7 @@
 //   console.log('Você não esta tendo lucro');
 // }
 
-// 11 - Uma pessoa que trabalha de carteira assinada no Brasil tem descontados de seu salário bruto o INSS e o IR. Faça um programa que, dado um salário bruto, calcule o líquido a ser recebido.
-// A notação para um salário de R$1500,10, por exemplo, deve ser 1500.10. Para as faixas de impostos, use as seguintes referências:
-// INSS (Instituto Nacional do Seguro Social)
-// Salário bruto até R$ 1.556,94: alíquota de 8%
-// Salário bruto de R$ 1.556,95 a R$ 2.594,92: alíquota de 9%
-// Salário bruto de R$ 2.594,93 a R$ 5.189,82: alíquota de 11%
-// Salário bruto acima de R$ 5.189,82: alíquota máxima de R$ 570,88
-// IR (Imposto de Renda)
-// Até R$ 1.903,98: isento de imposto de renda
-// De R$ 1.903,99 a 2.826,65: alíquota de 7,5% e parcela de R$ 142,80 a deduzir do imposto
-// De R$ 2.826,66 a R$ 3.751,05: alíquota de 15% e parcela de R$ 354,80 a deduzir do imposto
-// De R$ 3.751,06 a R$ 4.664,68: alíquota de 22,5% e parcela de R$ 636,13 a deduzir do imposto
-// Acima de R$ 4.664,68: alíquota de 27,5% e parcela de R$ 869,36 a deduzir do imposto.
+// ATIVIDADE 11
 
 // let inss;
 // let ir;
--- fundamentos/bloco4-int-javascript-logica-progamacao/dia2-javascript-array-e-loop-for/scripts.js
+++ fundamentos/bloco4-int-javascript-logica-progamacao/dia2-javascript-array-e-loop-for/scripts.js
@@ -1,5 +1,5 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-// 1 - Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log();
+// ATIVIDADE 1
 let n = -1;
 console.log('Exercicio 1');
 
@@ -8,7 +8,7 @@ for (let index = 0; index < numbers.length; index += 1) {
   console.log('Posição ' + n + ' - ' + numbers[index]);
 }
 
-// 2 - Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
+// ATIVIDADE 2
 let soma = 0;
 console.log('Exercicio 2');
 
@@ -17,7 +17,7 @@ for (let index = 0; index < numbers.length; index += 1) {
 }
 console.log(soma);
 
-// 3 - Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
+// ATIVIDADE 3
 // A média aritmética é o resultado da soma de todos os elementos divido pelo número total de elementos.
 let sum = 0;
 let mediaArit;
@@ -30,7 +30,7 @@ for (let index = 0; index < numbers.length; index += 1) {
 mediaArit = sum / ((numbers.length) - 1);
 console.log(Math.floor(mediaArit));
 
-// 4 - Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
+// ATIVIDADE 4
 console.log('Exercicio 4');
 
 if (mediaArit > 20) {
@@ -40,7 +40,7 @@ else {
   console.log('valor menor ou igual a 20');
 }
 
-// 5 - Utilizando for, descubra qual o maior valor contido no array e imprima-o;
+// ATIVIDADE 5
 console.log('Exercicio 5');
 
 let maior = 0;
@@ -51,7 +51,7 @@ for (let index = 0; index < numbers.length; index += 1) {
 }
 console.log(maior);
 
-// 6 - Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
+// ATIVIDADE 6
 console.log('Exercicio 6');
 
 let impar = 0;
@@ -66,7 +66,7 @@ if (impar === 0) {
   console.log('Nenhum valor ímpar encontrado!');
 }
 
-// 7 - Utilizando for, descubra qual o menor valor contido no array e imprima-o;
+// ATIVIDADE 7
 console.log('Exercicio 7');
 
 let menor = numbers[0];
@@ -78,7 +78,7 @@ for (let index = 0; index < numbers.length; index += 1) {
 }
 console.log(menor);
 
-// 8 - Utilizando for, crie um array que vá de 1 até 25 e imprima o resultado;
+// ATIVIDADE 8
 console.log('Exercicio 8');
 
 let newArray = [];
@@ -89,7 +89,7 @@ for (let index = 0; index < 25; index += 1) {
 console.log(newArray);
 
 
-// 9 - Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2.
+// ATIVIDADE 9
 console.log('Exercicio 9');
 
 let results = 0;
@@ -99,16 +99,7 @@ for (let index = 0; index < newArray.length; index += 1) {
 }
 
 // >>>>>>   BONUS <<<<<<
-// Para os próximos dois exercícios leia este artigo e tente entender o que está acontencedo no código abaixo:
-// for (let index = 1; index < array.length; index += 1) {
-//   for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
-//     if (array[index] < array[secondIndex]) {
-//       let position = array[index];
-//       array[index] = array[secondIndex];
-//       array[secondIndex] = position;
-//     }
-//   }
-// }
+// BONUS 1
 
 let numbers2 = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // 1 - Ordene o array numbers2 em ordem crescente e imprima seus valores;
@@ -125,7 +116,7 @@ for (let i = 1; i < numbers2.length; i += 1) {
 }
 console.log(numbers2);
 
-// 2 - Ordene o array numbers em ordem decrescente e imprima seus valores;
+// BONUS 2
 let numbers3 = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 console.log('Exercicio Bonus - 2');
 
@@ -140,8 +131,7 @@ for (let i = numbers3.length - 2; i >= 0; i -= 1) {
 }
 console.log(numbers3);
 
-// 3 - Agora crie um novo array a partir do array numbers, sem perdê-lo. Cada valor do novo array deverá ser igual ao valor correspondente no array numbers multiplicado pelo seguinte. Por exemplo: o primeiro valor do novo array deverá ser 45, pois é a multiplicação de 5 (primeiro valor) e 9 (valor seguinte). Já o segundo valor do novo array deverá ser 27, pois é a multiplicação de 9 (segundo valor) e 3 (valor seguinte), e assim por diante. Caso não haja próximo valor, a multiplicação deverá ser feita por 2. Faça isso utilizando o for e o método push. O resultado deve ser o array abaixo:
-
+// BONUS 3
 
 // [45, 27, 57, 1330, 560, 800, 200, 70, 945, 54]
 
