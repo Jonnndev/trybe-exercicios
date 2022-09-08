6e84d416c10bbf92caef9c104eb2ffe6d6d6c955
Jonathas Assis de Oliveira <jonnutri@gmail.com>
2022-09-08 10:42:24 -0300

    Exercicios 6.2 - Testando bibliotecas

---
+++ fundamentos/bloco6-html-css-forms-flexbox-responsivo/dia2-bibliotecasJS-frameworksCSS/script.js
@@ -0,0 +1,17 @@
+// SELETORES
+const btnSend = document.getElementById('btnSend');
+const checkBoxImage = document.getElementById('input-checkImage');
+const inputEmailValue = document.getElementById('input-email').value;
+
+function noSend(event) {
+  event.preventDefault();
+}
+btnSend.addEventListener('click', noSend);
+
+function check() {
+  if (checkBoxImage.checked === false) {
+    alert('Aceita divulgar suas imagens?');
+    event.preventDefault();
+  }
+}
+btnSend.addEventListener('click', check);
