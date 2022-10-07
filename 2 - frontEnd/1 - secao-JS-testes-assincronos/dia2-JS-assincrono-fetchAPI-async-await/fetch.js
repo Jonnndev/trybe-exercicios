8a291e17f988a45b307cab2e6d8e3488eae4865f
Jonathas Assis de Oliveira <jonnutri@gmail.com>
2022-10-07 19:18:46 -0300

    Materia e Exercicios FrontEnd 1.2

---
+++ 2 - frontEnd/1 - secao-JS-testes-assincronos/dia2-JS-assincrono-fetchAPI-async-await/fetch.js
@@ -0,0 +1,71 @@
+// const fetch = require('node-fetch');
+
+// const fetchJoke = () => {
+//   const url = 'api.chucknorris.io/jokes/random?category=dev';
+
+//   fetch(url)
+//     .then((response) => response.json())
+//     .then((data) => console.log(data.value))
+//     .catch((error) => console.log(`Algo deu errado :( \n${error}`));
+// }
+
+// fetchJoke();
+
+// Algo deu errado :( 
+// TypeError: Only absolute URLs are supported
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> ASYNC E AWAIT <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+// const fetch = require('node-fetch');
+
+// const fetchJoke = async () => {
+//   const url = 'https://api.chucknorris.io/jokes/random?category=dev';
+
+//   fetch(url)
+//     .then((response) => response.json())
+//     .then((data) => console.log(data.value))
+//     .catch((error) => console.log(`Algo deu errado :( \n${error}`));
+// }
+
+// fetchJoke();
+
+// Chuck Norris can write multi-threaded applications with a single thread.
+
+// 1- MESCLANDO
+
+// const fetch = require('node-fetch');
+
+// const fetchJoke = async () => {
+//   const url = 'https://api.chucknorris.io/jokes/random?category=dev';
+
+//   const result = await fetch(url)
+//     .then((response) => response.json())
+//     .then((data) => data.value)
+//     .catch((error) => `Algo deu errado :( \n${error}`);
+  
+//   console.log(result);
+// };
+
+// fetchJoke();
+
+// Chuck Norris can write multi-threaded applications with a single thread.
+
+// 2 - REFATORAND0
+
+const fetch = require('node-fetch');
+
+const fetchJoke = async () => {
+  const url = 'https://api.chucknorris.io/jokes/random?category=dev';
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data.value);
+  } catch(error) {
+    console.log(`Algo deu errado :( \n${error}`);
+  }
+}
+
+fetchJoke();
+
+// Chuck Norris can write multi-threaded applications with a single thread.
