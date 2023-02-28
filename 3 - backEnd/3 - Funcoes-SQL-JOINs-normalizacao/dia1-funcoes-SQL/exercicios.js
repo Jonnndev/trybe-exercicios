604b7dc5ca97552a1ca569c8823c04472227d382
Jonathas Oliveira <jonnutri@gmail.com>
2023-02-28 20:59:55 -0300

    Seção 3 - Dia 1: Exercicios

--- 3 - backEnd/3 - Funcoes-SQL-JOINs-normalizacao/dia1-funcoes-SQL/exercicios.js
+++ 3 - backEnd/3 - Funcoes-SQL-JOINs-normalizacao/dia1-funcoes-SQL/exercicios.js
@@ -23,7 +23,8 @@
 --> SELECT SUM(salary) AS Folha_de_Pagamento
     FROM hr.employees;
 
-5 - Escreva uma query que exiba quatro informações: o maior salário, o menor salário, a soma de todos os salários e a média dos salários. Todos os valores devem ser formatados para ter apenas duas casas decimais.
+5 - Escreva uma query que exiba quatro informações: o maior salário, o menor salário, a soma de todos os salários e a média
+dos salários. Todos os valores devem ser formatados para ter apenas duas casas decimais.
 
 --> SELECT MAX(salary) AS Maior_Salario,
     MIN(salary) AS Menor_Salario,
@@ -49,14 +50,16 @@
     FROM hr.employees
     GROUP BY job_id;
 
-8 - Utilizando a query anterior, faça as alterações para que seja exibido somente a quantidade de dinheiro necessária para cobrir a folha de pagamento das pessoas programadoras (it_prog).
+8 - Utilizando a query anterior, faça as alterações para que seja exibido somente a quantidade de dinheiro necessária para cobrir
+a folha de pagamento das pessoas programadoras (it_prog).
 
 --> SELECT job_id,
     SUM(salary) AS Folha_Pagamento_Vinculo
     FROM hr.employees
     WHERE job_id = 'IT_PROG';
 
-9 - Escreva uma query que exiba em ordem decrescente a média salarial de todos os cargos, exceto das pessoas programadoras (it_prog).
+9 - Escreva uma query que exiba em ordem decrescente a média salarial de todos os cargos, exceto das pessoas programadoras
+(it_prog).
 
 --> SELECT job_id,
     SUM(salary) AS Folha_Pagamento_Vinculo
@@ -65,7 +68,8 @@
     GROUP BY job_id
     ORDER BY Folha_Pagamento_Vinculo DESC;
 
-10 -  Escreva um query que exiba média salarial e o número de funcionários de todos os departamentos com mais de dez funcionários. Dica: agrupe pelo department_id.
+10 -  Escreva um query que exiba média salarial e o número de funcionários de todos os departamentos com mais de dez funcionários.
+ Dica: agrupe pelo department_id.
 
 --> SELECT department_id,
     COUNT(*) AS Qntd_Funcionarios,
@@ -74,7 +78,8 @@
     GROUP BY department_id
     HAVING Qntd_Funcionarios > 10;
 
-11 - Escreva uma query que atualize a coluna phone_number, de modo que todos os telefones iniciados por 515 agora devem iniciar com 777.
+11 - Escreva uma query que atualize a coluna phone_number, de modo que todos os telefones iniciados por 515 agora devem iniciar 
+com 777.
 
 --> SELECT REPLACE(phone_number, 515, 777)
     FROM hr.employees;
@@ -85,21 +90,24 @@
     FROM hr.employees
     WHERE CHAR_LENGTH(first_name) >= 8;
 
-13 - Escreva uma query que exiba as seguintes informações de cada funcionário: id, primeiro nome e ano no qual foi contratado (exiba somente o ano).
+13 - Escreva uma query que exiba as seguintes informações de cada funcionário: id, primeiro nome e ano no qual foi contratado
+(exiba somente o ano).
 
 --> SELECT employee_id AS id,
     first_name AS Primeiro_Nome, 
     YEAR(hire_date) AS Ano_Contratacao
     FROM hr.employees;
 
-14 - Escreva uma query que exiba as seguintes informações de cada funcionário: id, primeiro nome e dia do mês no qual foi contratado (exiba somente o dia).
+14 - Escreva uma query que exiba as seguintes informações de cada funcionário: id, primeiro nome e dia do mês no qual foi
+contratado (exiba somente o dia).
 
 --> SELECT employee_id AS id,
     first_name AS Primeiro_Nome, 
     DAY(hire_date) AS Dia_Contratacao
     FROM hr.employees;
 
-15 - Escreva uma query que exiba as seguintes informações de cada funcionário: id, primeiro nome e mês no qual foi contratado (exiba somente o mês).
+15 - Escreva uma query que exiba as seguintes informações de cada funcionário: id, primeiro nome e mês no qual foi contratado 
+(exiba somente o mês).
 
 --> SELECT employee_id AS id,
     first_name AS Primeiro_Nome, 
@@ -118,7 +126,8 @@ FROM hr.employees;
     FROM hr.employees
     WHERE MONTH(hire_date) = 7;
 
-18 - Escreva uma query que exiba as seguintes informações de cada funcionário: nome, sobrenome, tempo que trabalha na empresa (em dias).
+18 - Escreva uma query que exiba as seguintes informações de cada funcionário: nome, sobrenome, tempo que trabalha na empresa
+(em dias).
 
 --> SELECT first_name AS Nome,
     last_name AS Sobrenome,
