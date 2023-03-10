652416681a2ea27b31eff6a0425b41b3b9a348b2
Jonathas Oliveira <jonnutri@gmail.com>
2023-03-09 21:56:54 -0300

    Seção  4 - Dia 3: Exercicios

--- 3 - backEnd/4 - int-dev-web-nodeJS/dia3-Node-Teste-de-integracao/cacau-trybe/src/app.js
+++ 3 - backEnd/4 - int-dev-web-nodeJS/dia3-Node-Teste-de-integracao/cacau-trybe/src/app.js
@@ -1,12 +1,26 @@
 const express = require('express');
+const cacauTrybe = require('./cacauTrybe');
 
 const app = express();
-const cacauTrybe = require('./cacauTrybe');
+app.use(express.json());
+
 
 //ENDPOINTS
 app.get('/chocolates', async (req, res) => {
   const chocolates = await cacauTrybe.getAllChocolates();
-  res.status(200).json({ chocolates });
+  return res.status(200).json({ chocolates });
+});
+
+app.get('/chocolates/total', async (_req, res) => {
+const totalChocolates = await cacauTrybe.getAllChocolates();
+return res.status(200).json({ totalChocolates: totalChocolates.length });
+});
+
+app.get('/chocolates/search', async (req, res) => {
+  const { name} = req.query;
+  const chocolates = await cacauTrybe.findChocolatesByName(name);
+  if (chocolates.length === 0) return res.status(404).json([]);
+  return res.status(200).json(chocolates);
 });
 
 app.get('/chocolates/:id', async (req, res) => {
@@ -14,13 +28,22 @@ app.get('/chocolates/:id', async (req, res) => {
   // Usamos o Number para converter o id em um inteiro
   const chocolate = await cacauTrybe.getChocolateById(Number(id));
   if (!chocolate) return res.status(404).json({ message: 'Chocolate not found' });
-  res.status(200).json({ chocolate });
+  return res.status(200).json(chocolate);
 });
 
 app.get('/chocolates/brand/:brandId', async (req, res) => {
   const { brandId } = req.params;
   const chocolates = await cacauTrybe.getChocolatesByBrand(Number(brandId));
-  res.status(200).json({ chocolates });
+  return res.status(200).json({ chocolates });
+});
+
+app.put('/chocolates/:id', async (req, res) => {
+  const { id } = req.params;
+  const { name, brandId } = req.body;
+  const updatedChocolate = await cacauTrybe.updateChocolate(Number(id), { name, brandId });
+
+  if (updatedChocolate) return res.status(200).json({ chocolate: updatedChocolate });
+  return res.status(404).json({ message: 'chocolate not found' });
 });
 
 module.exports = app;
--- 3 - backEnd/4 - int-dev-web-nodeJS/dia3-Node-Teste-de-integracao/cacau-trybe/src/cacauTrybe.js
+++ 3 - backEnd/4 - int-dev-web-nodeJS/dia3-Node-Teste-de-integracao/cacau-trybe/src/cacauTrybe.js
@@ -1,14 +1,42 @@
 const fs = require('fs').promises;
 const { join } = require('path');
+const path = '/files/cacauTrybeFile.json';
 
 const readCacauTrybeFile = async () => {
-  const path = '/files/cacauTrybeFile.json';
   try {
     const contentFile = await fs.readFile(join(__dirname, path), 'utf-8');
     return JSON.parse(contentFile);
   } catch (error) {
     return null;
   }
+};
+
+const writeCacauTrybeFile = async (content) => {
+  try {
+    const completePath = join(__dirname, path);
+    await fs.writeFile(completePath, JSON.stringify(content));
+  } catch (error) {
+    return console.error('Erro ao salvar o arquivo', error.message);
+  }
+}
+
+const updateChocolate = async (id, update) => {
+  const cacauTrybe = await readCacauTrybeFile();
+  const chocolateToUpdate = cacauTrybe.chocolates.find(
+    (chocolate) => chocolate.id === id,
+  );
+
+  if (chocolateToUpdate) {
+    cacauTrybe.chocolates = cacauTrybe.chocolates.map((chocolate) => {
+        if (chocolate.id === id) return { ...chocolate, ...update };
+        return chocolate;
+      });
+  
+    await writeCacauTrybeFile(cacauTrybe, null, 2);
+    return { ...chocolateToUpdate, ...update };
+  }
+
+  return false;
 };
 
 const getAllChocolates = async () => {
@@ -27,8 +55,16 @@ const getChocolatesByBrand = async (brandId) => {
   return cacauTrybe.chocolates
     .filter((chocolate) => chocolate.brandId === brandId);
 };
+
+const findChocolatesByName = async (name) => {
+  const cacauTrybe = await readCacauTrybeFile();
+  return cacauTrybe.chocolates.filter((item) => item.name.toLowerCase().includes(name.toLowerCase()));
+};
+
 module.exports = {
     getAllChocolates,
     getChocolateById,
-    getChocolatesByBrand
+    getChocolatesByBrand,
+    findChocolatesByName,
+    updateChocolate
 };
