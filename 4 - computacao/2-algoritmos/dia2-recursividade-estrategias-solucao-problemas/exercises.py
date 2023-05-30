f9b39e3b3c6d4f14a88ee8e7c620295a8137e7b4
jonnoliveira <jonnutri@gmail.com>
2023-05-30 12:26:47 -0300

    Add: Seção 2 - Dia 2: exercicios

--- 4 - computacao/2-algoritmos/dia2-recursividade-estrategias-solucao-problemas/exercises.py
+++ 4 - computacao/2-algoritmos/dia2-recursividade-estrategias-solucao-problemas/exercises.py
@@ -14,3 +14,70 @@
 
 
 # recursive_sum(4)
+
+# Exercicio 1 - Crie um algoritmo não recursivo para contar quantos números pares existem em uma sequência numérica (1 a n).
+print("Exercicio 1")
+
+
+def iterative_sum(n):
+    a = list(range(1, n + 1))
+    index = 0
+    sum = 0
+    while index < len(a):
+        if a[index] % 2 == 0 and n != 0:
+            sum += 1
+        index += 1
+    return sum
+
+
+print(f"Qntds de números pares é:", iterative_sum(20))
+
+# Exercicio 2 -  Transforme o algoritmo criado acima em recursivo
+print("Exercicio 2")
+
+
+def recursive_sum(n):
+    if n == 1:
+        return 0
+    elif n % 2 == 0:
+        return 1 + recursive_sum(n - 1)
+    else:
+        return recursive_sum(n - 1)
+
+
+print(f"Qntds de números pares é:", recursive_sum(10))
+
+# Exercicio 3 -   Crie um algoritmo recursivo que encontre, em uma lista, o maior número inteiro.
+print("Exercicio 3")
+
+
+def maiorinteiro_aux(lista, tamanho):
+    if tamanho == 1:
+        return lista[0]
+    else:
+        maior_do_resto_da_lista = maiorinteiro_aux(lista, tamanho - 1)
+        if maior_do_resto_da_lista > lista[tamanho - 1]:
+            return maior_do_resto_da_lista
+        else:
+            return lista[tamanho - 1]
+
+
+def maiorinteiro(lista):
+    tamanho = len(lista)
+    return maiorinteiro_aux(lista, tamanho)
+
+
+print(f"Maior número é:", maiorinteiro([1, 21, 300, 4, 57]))
+
+# Exercicio 4 - Escreva um algoritmo recursivo para encontrar o máximo divisor comum (mdc) de
+# dois inteiros.
+print("Exercicio 4")
+
+
+def mdc(a, b):
+    if b == 0:
+        return a
+    return mdc(b, a % b)
+
+
+print(f"MDC:", mdc(4, 16))
