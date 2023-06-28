06bece6e238ec9a159c2f08d30e1ecadff179f08
jonnoliveira <jonnutri@gmail.com>
2023-06-28 18:06:55 -0300

    Add: Seção 1 - Dia 1: Materia

---
+++ 5 - eletiva-e-aceleracoes/python/s1-debug-com-python/dia1-debug-com-python/materia.js
@@ -0,0 +1,25 @@
+============================================= DEBUG COM PYTHON =============================================
+
+Uma forma de debugar um código é utilizando prints ou logs. No entanto, essa estratégia pode ser bastante
+ineficiente, pois você precisa adicionar e remover esses prints a todo momento, além de ter que executar o
+código diversas vezes para encontrar o erro.
+
+É nesse momento que a ferramenta de depuração (debug) do VS Code se torna uma grande aliada. Ela permite que
+você interaja com o código enquanto ele está sendo executado, parando a execução em pontos específicos para que
+você possa analisar o estado das variáveis, encontrar problemas e corrigi-los de forma mais rápida e eficiente.
+
+============================================= VISUALIZANDO DADOS =============================================
+
+1 - VARIABLES (variáveis)
+2 - CALL STACK (pilha de chamadas)
+3 - WATCH (inspeção)
+4 - BREAKPOINTS (pontos de parada)
+
+--> VARIABLES: Nessa janela poderemos ver todas As variáveis e os seus valores no contexto atual da aplicação.
+
+--> CALL STACK: Essa janela mostra quais funções/métodos/módulos foram chamados para que a execução chegasse até
+a linha atual.
+
+--> WATCH: A janela WATCH permite que você inspecione o valor de uma expressão ou variável específica.
+
+--> BREAKPOINTS: A janela BREAKPOINTS permite que você adicione, remova e gerencie os pontos de parada do seu código.
