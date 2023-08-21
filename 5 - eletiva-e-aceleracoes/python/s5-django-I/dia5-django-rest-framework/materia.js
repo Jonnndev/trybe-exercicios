814682fced038a9b5b29d37c07c21b5ab049bf66
Jonn <jonnutri@gmail.com>
2023-08-21 12:05:08 -0300

    feat: Seção 5 - Dia 5: Materia

---
+++ 5 - eletiva-e-aceleracoes/python/s5-django-I/dia5-django-rest-framework/materia.js
@@ -0,0 +1,18 @@
+======================================= DJANGO REST FRAMEWORK =======================================
+
+
+1 - Suas políticas de autenticação, que incluem pacotes para OAuth1a e OAuth2.
+2 - Serialização (processo de conversão de objetos Python em formatos como JSON, XML ou YAML, por exemplo)
+    que suporta fontes de dados baseadas em ORM (Object-Relational Mapping) ou não.
+3 - Completamente personalizável - basta usar views comuns baseadas em funções caso você não precise dos recursos mais poderosos.
+4 - Documentação extensa e grande suporte da comunidade.
+5 - Utilizado por empresas reconhecidas internacionalmente, incluindo Mozilla, Red Hat e Heroku.
+
+--> pip install django djangorestframework
+--> pip install markdown django-filter mysqlclient
+
+# SERIALIZERS:
+
+Um serializador de dados no DRF é o que permite converter objetos Python em formatos como JSON, XML, YAML, entre outros.
+Isso é importante para que os dados sejam enviados e recebidos de forma estruturada e legível por diferentes sistemas e
+clientes. Resumidamente, isso garante maior compatibilidade, controle de dados e facilita as validações.
